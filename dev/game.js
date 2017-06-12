81c589719ab30873300fd318ccb9364e2441147b
sleepokay <sleepokay@gmail.com>
2017-06-12 17:40:35 -0400

    Add display sections; timing

--- dev/game.js
+++ dev/game.js
@@ -1,17 +1,23 @@
+// player contains the variables related to population
 var player;
 
+// environmental variables
 var sun;
 var rain;
+var chance = [-1, 1];
 
+// time variables
 var delta = 100; // in milliseconds
 var timeElapsed;
 var year;
 var season;
 
-const SEASONS = ["spring 1", "spring 2", "spring 3", 
-					"summer 1", "summer 2", "summer 3", 
-					"fall 1", "fall 2", "fall 3",
-					"winter 1", "winter 2", "winter 3"];
+const yearLength = 12000;
+const MONTHS = ["Ixo", "Nexo", "Sah", "Shodo", "Goru", "Loxu", "Xin", "Bah", "Noru", "Jio", "Jixo", "Jinx"];
+const SEASONS = ["spring", "spring", "spring", 
+					"summer", "summer", "summer", 
+					"fall", "fall", "fall",
+					"winter", "winter", "winter"];
 
 $(document).ready( function() {
 	newGame();
@@ -33,20 +39,25 @@ function draw() {
 	var stats = "";
 	stats += "Time Elapsed: " + timeElapsed;
 	stats += "<br>Year: " + year;
+	stats += "<br>Month " + (season+1) + ": " + MONTHS[season];
 	stats += "<br>Season: " + SEASONS[season];
-	$('#main').html(stats);
+	$('#environment').html(stats);
 
-	$('#people').html("People: " + player.people);
-	$('#slaves').html("Slaves: " + player.slaves);
-	$('#grain').html("Grain: " + player.grain);
+	stats = "Population: " + player.popCount();
+
+	for (var key in player.population) {
+		stats += "<br>" + key + ": " + player.population[key].length;
+	}
+
+	$('#population').html(stats);
 }
 
 function update() {
 	timeElapsed += delta;
 
-	if (timeElapsed % 12000 == 0)
+	if (timeElapsed % yearLength == 0)
 		year++;
-	if (timeElapsed > 12000)
-		timeElapsed -= 12000;
-	season = Math.floor(timeElapsed%12000/1000);
+	if (timeElapsed > yearLength)
+		timeElapsed -= yearLength;
+	season = Math.floor(timeElapsed%yearLength/1000);
 }
--- dev/player.js
+++ dev/player.js
@@ -1,7 +1,62 @@
-function Player() {
-	this.grain = 50;
-	this.people = 200;
-	this.slaves = 0;
-	this.foodProduction = 1;
-	this.chance = [-1, 1];
+class Player {
+	constructor() {
+		this.grain = 100;
+		this.productivity = 1;
+
+		this.population = {};
+		this.initialize(50);
+	}
+
+	initialize(size) {
+		// temporary: should find better way to represent 
+		// population/age structure for easier sampling
+		var distribution = {"children": 6,
+							"warriors": 10,
+							"farmers": 21,
+							"mothers": 6,
+							"maidens": 4,
+							"elders": 3}
+
+		var temp = {"children": [],
+					"warriors": [],
+					"farmers": [],
+					"mothers": [],
+					"maidens": [],
+					"elders": []};
+
+		for (var key in temp) {
+			console.log(key);
+			for (var n = 0; n < distribution[key]; n++) {
+				temp[key].push(new Person(key));
+			}
+		}
+
+		this.population = temp;
+	}
+
+	popCount() {
+		var count = 0;
+		for (var key in this.population) {
+			count += this.population[key].length;
+		}
+		return count;
+	}
+}
+
+class Person {
+	constructor(type) {
+		var range = [];
+
+		if (type == "children")
+			range = [0, 13];
+		else if (type == "elders")
+			range = [50, 75];
+		else
+			range = [13, 50];
+		this.age = Math.floor(Math.random() * (range[1] - range[0]) + range[0]);
+	}
+
+	isChild() {
+		return this.age < 13;
+	}
 }
